6c690a21a8c2f262e2a3c49835c3fef0edec296c
Subhajit Sahu <wolfram77@gmail.com>
2018-11-09 13:06:43 +0530

    readme: gives usage example

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-Decode, encode, transcode, mux, demux, stream, filter, and play media through machine (via "ffmpeg").
+Decode, encode, transcode, mux, demux, stream, filter, and play media through machine (via ["ffmpeg"]).
+> Uses [setup-ffmpeg] to install, if absent.
+
+```javascript
+const ffmpeg = require('extra-ffmpeg');
+// ffmpeg.sync(<options>): stdout when done
+// ffmpeg(<options>)
+// -> Promise {stdout, stderr} when done
+
+// <options>: [{ // see ffmpeg -h
+//   outfile|o: // name of output file
+// }]
+
+
+ffmpeg.sync([{y: true}, {i: 'concat:0.mp3|1.mp3'}, {acodec: 'copy', o: 'aud.mp3'}]);
+// concat 0.mp3, 1.mp3 to aud.mp3
+
+ffmpeg.sync([
+  {y: true, err_detect: 'explode'}, {loop: 1, framerate: 1}, {i: 'img.jpg'}, {i: 'aud.mp3'},
+  {vcodec: 'libx264', crf: 0, preset: 'veryfast', tune: 'stillimage',
+    vf: 'scale=trunc(iw/2)*2:trunc(ih/2)*2', acodec: 'copy', shortest: true, o: 'vid.mp4'}
+]);
+// get video vid.mp4 from still image img.jpg and audio aud.mp4
+```
+
+
+[![Merferry](https://i.imgur.com/HS08T0y.jpg)](https://merferry.github.io)
+
+["ffmpeg"]: https://ffmpeg.org
+[setup-ffmpeg]: https://www.npmjs.com/package/setup-ffmpeg
--- index.js
+++ index.js
@@ -6,34 +6,38 @@ const STDIO = [0, 1, 2];
 
 
  // Generate command for ffmpeg.
- function command(o) {
+ function command(os) {
   var z = 'ffmpeg';
-  var o = o||{}, stdio = STDIO;
-  for(var k in o) {
-    if(o[k]==null) continue;
-    if(k==='stdio') continue;
-    if(typeof o[k]==='boolean') z += o[k]? ` -${k}`:'';
-    else z += ` -${k} ${JSON.stringify(o[k])}`;
+  var os = os||[];
+  for(var o of os) {
+    var o = o||{};
+    for(var k in o) {
+      if(o[k]==null) continue;
+      if(k==='stdio') continue;
+      if(k==='o' || k==='outfile') z += `"${o[k]}"`;
+      else if(typeof o[k]==='boolean') z += o[k]? ` -${k}`:'';
+      else z += ` -${k} ${JSON.stringify(o[k])}`;
+    }
   }
   return z;
 };
 
 /**
  * Invoke "ffmpeg" synchronously.
- * @param {object} o ffmpeg options.
+ * @param {object} os ffmpeg options.
  */
-function sync(o) {
-  var stdio = o.stdio===undefined? STDIO:o.stdio;
-  return cp.execSync(command(o), {stdio});
+function sync(os) {
+  var stdio = os.stdio===undefined? STDIO:os.stdio;
+  return cp.execSync(command(os), {stdio});
 };
 
 /**
  * Invoke "ffmpeg" asynchronously.
- * @param {object} o ffmpeg options.
+ * @param {object} os ffmpeg options.
  */
-function ffmpeg(o) {
-  var stdio = o.stdio===undefined? STDIO:o.stdio;
-  return new Promise((fres, frej) => cp.exec(command(o), {stdio}, (err, stdout, stderr) => {
+function ffmpeg(os) {
+  var stdio = os.stdio===undefined? STDIO:os.stdio;
+  return new Promise((fres, frej) => cp.exec(command(os), {stdio}, (err, stdout, stderr) => {
     if(err) frej(err);
     else fres({stdout, stderr});
   }));
