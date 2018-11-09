15e8f81092e43b2eddbc7168cf3e268893124d25
Subhajit Sahu <wolfram77@gmail.com>
2018-11-09 12:41:35 +0530

    install: do it if not exists

---
+++ install.js
@@ -0,0 +1,20 @@
+const cp = require('child_process');
+
+
+// Run a command, return true if success.
+function cpRun(txt) {
+  try { cp.execSync(txt, {stdio: []}); }
+  catch(e) { return false; }
+  return true;
+};
+
+// Setup "ffmpeg".
+function setup() {
+  if(cpRun('ffmpeg -version')) {
+    console.log('extra: ffmpeg already exists.');
+    return;
+  }
+  console.log('extra: Using setup-ffmpeg');
+  cp.execSync('npm install -g setup-ffmpeg', {stdio: [0, 1, 2]});
+};
+setup();
